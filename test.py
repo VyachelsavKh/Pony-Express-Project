daddf210366e75c19ed10eee2e39c20c1c208096
Vyacheslav <61741665+VyachelsavKh@users.noreply.github.com>
2020-09-25 18:26:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,15 @@ def dupl(a):
             return 1
     return 0
 
-print(dupl([1,2,3,4,5]))
+class Rectangle:
+    def __init__(self, list_of_dots = []):
+        self.dots = list_of_dots
+    def perimetr(self):
+        return self.dots[1][1] - self.dots[0][1] + self.dots[2][0] - self.dots[1][0] + self.dots[2][1] - self.dots[3][1] + self.dots[3][0] - self.dots[0][0]
+    def square(self):
+        return (self.dots[1][1] - self.dots[0][1])*(self.dots[2][0] - self.dots[1][0])
+
+a = Rectangle([[0,0],[0,5],[5,5],[5,0]])
+
+print(a.perimetr())
+print(a.square())
