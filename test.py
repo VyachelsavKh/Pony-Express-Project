86a3831d01c4bb930d50764e3dd28ce18db5d8e8
Vyacheslav <61741665+VyachelsavKh@users.noreply.github.com>
2020-09-25 18:12:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-def ret(a):
-    return [a[0], a[-1]]
+def dupl(a):
+    a.sort()
+    for i in range(1, len(a)):
+        if a[i] == a[i-1]:
+            return 1
+    return 0
 
-print(ret([1,2,3,4,5]))
+print(dupl([1,2,3,4,5]))
