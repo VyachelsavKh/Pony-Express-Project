7bcc0b8e16b1667e203608cc0077eba02dbfccce
Vyacheslav <61741665+VyachelsavKh@users.noreply.github.com>
2020-10-28 16:23:45 +0300

    Add files via upload

--- test_10.py
+++ test_10.py
@@ -1,4 +1,5 @@
 import waitings
+import time
 import log_output
 import login_page
 import arrived_at_the_warehouse_no_sorting
@@ -11,17 +12,35 @@ def test_10():
 
     if identifiers == 'Wrong identifiers':
         log_output.Print('Тест 10 не пройден')
+        waitings.visual_checking()
+        driver.close()
+        driver.switch_to_window(driver.window_handles[0])
+        driver.close()
+        return
 
     result = arrived_at_the_warehouse_no_sorting.check_menu(driver)
 
     if result == 'ERROR':
         log_output.Print('Тест 10 не пройден')
+        waitings.visual_checking()
+        driver.close()
+        driver.switch_to_window(driver.window_handles[0])
+        driver.close()
+        return
 
     name = '11-1111-1112'
 
     arrived_at_the_warehouse_no_sorting.enter_object_number(driver, name)
 
-    waitings.searching()
+    if result == 'ERROR':
+        log_output.Print('Тест 10 не пройден')
+        waitings.visual_checking()
+        driver.close()
+        driver.switch_to_window(driver.window_handles[0])
+        driver.close()
+        return
+
+    time.sleep(1)
 
     back_ground_colour = arrived_at_the_warehouse_no_sorting.check_colour(driver)
 
@@ -36,6 +55,7 @@ def test_10():
     driver.close()
     driver.switch_to_window(driver.window_handles[0])
     driver.close()
+    return
 
 if __name__ == "__main__":
     test_10()
